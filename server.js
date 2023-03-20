c71f714f1c70f4181385140fffd62b70be5aa716
casey-tk-cheung <62992468+casey-tk-cheung@users.noreply.github.com>
2023-03-20 11:00:38 +0000

    Update server.js

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ headers.append('X-ApiKey', 'AA26F453-D34D-4EFC-9DC8-F63625B67F4A');
 headers.append('X-ApiVersion', '1');
 
 //static asset directories
-app.use(express.static(__dirname + '/..' + '/frontend'))
+//app.use(express.static(__dirname + '/..' + '/frontend'))
 //app.use(express.static(__dirname + '/..' + '/backend'))
 
-app.get('/', (req, res) => 
-{
+//app.get('/', (req, res) => 
+//{
   //getWorkingTiplocs(jsonFile, 25, '2023-02-16', '2023-02-16', headers)
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
-})
+  //res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
+//})
 
 
 app.listen(port, () =>{
