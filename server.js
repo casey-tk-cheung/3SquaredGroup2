ba9a99f48a011e3463e0bf06ba33e3fb0c4df9ce
casey-tk-cheung <62992468+casey-tk-cheung@users.noreply.github.com>
2023-03-20 11:19:27 +0000

    Update server.js

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ headers.append('X-ApiKey', 'AA26F453-D34D-4EFC-9DC8-F63625B67F4A');
 headers.append('X-ApiVersion', '1');
 
 //static asset directories
-app.use(express.static(__dirname + '/..' + '3SQUARED_Project/frontend'))
-app.use(express.static(__dirname + '/..' + '3SQUARED_Project/backend'))
+app.use(express.static(__dirname + '/..' + 'frontend'))
+app.use(express.static(__dirname + '/..' + 'backend'))
 
 app.get('/', (req, res) => 
 {
   //getWorkingTiplocs(jsonFile, 25, '2023-02-16', '2023-02-16', headers)
-  res.sendFile(path.join(__dirname, '..', 'frontend', '3SQUARED_Project/index.html'));
+  res.sendFile(path.join(__dirname, '..', '3SQUARED_Project/frontend', 'index.html'));
 })
 
 
